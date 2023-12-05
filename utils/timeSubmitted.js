65054c62c5bf07be8cff7dcca8d6f57185c2ea0c
MaxRubel <maxrubel1@gmail.com>
2023-12-05 15:54:09 -0600

    sort by date working

--- utils/timeSubmitted.js
+++ utils/timeSubmitted.js
@@ -1,11 +1,13 @@
 const timeSubmitted = () => {
   const date = new Date();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const month = date.getMonth();
-  const day = date.getDate();
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  const month = date.getMonth() + 1;
+  const monthF = month.toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
   const year = date.getFullYear();
-  return `${month + 1}/${day}/${year} ${hours}:${minutes}`;
+  return [`${month}${day}${year} ${hours}${minutes}${seconds}`, `${monthF}/${day}/${year} ${hours}:${minutes}:${seconds}`];
 };
 
 export default timeSubmitted;
