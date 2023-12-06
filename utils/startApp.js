4fc5eac57a7b7b723b36b309d45fdb9638e2b5c6
MaxRubel <maxrubel1@gmail.com>
2023-12-06 00:52:31 -0600

    stuff working well

--- utils/startApp.js
+++ utils/startApp.js
@@ -6,6 +6,7 @@ import formEvents from '../events/formEvents';
 import navigationEvents from '../events/navigationEvents';
 import { getCards } from '../api/cardsData';
 import { showCards } from '../pages/cards';
+// import { showCards } from '../pages/cards';
 
 const startApp = (user) => {
   domBuilder(); // BUILD THE DOM
@@ -14,9 +15,7 @@ const startApp = (user) => {
   navBar(); // DYNAMICALLY ADD THE NAV
   logoutButton(); // ADD THE LOGOUT BUTTON COMPONENT
   navigationEvents(user); // ATTACH THE EVENT LISTENERS TO THE NAVBAR
-  getCards(user).then((data) => {
-    showCards(data);
-  });
+  getCards(user).then(showCards);
 };
 
 export default startApp;
